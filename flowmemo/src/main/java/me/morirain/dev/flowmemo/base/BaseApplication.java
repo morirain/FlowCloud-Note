742f1cdcbdbfe34b37fc9a063417f858dc9eb59f
morirain <morirain@outlook.com>
2018-06-10 14:19:43 +0800

    * Change to ObjectBox

--- flowmemo/src/main/java/me/morirain/dev/flowmemo/base/BaseApplication.java
+++ flowmemo/src/main/java/me/morirain/dev/flowmemo/base/BaseApplication.java
@@ -4,6 +4,11 @@ package me.morirain.dev.flowmemo.base;
 import android.app.Application;
 import android.content.Context;
 
+import io.objectbox.BoxStore;
+import io.objectbox.android.AndroidObjectBrowser;
+import me.morirain.dev.flowmemo.BuildConfig;
+import me.morirain.dev.flowmemo.bean.MyObjectBox;
+
 /**
  * Created by morirain on 2018/6/2.
  * E-Mail Address：morirain.dev@outlook.com
@@ -18,9 +23,18 @@ public class BaseApplication extends Application {
         return appContext;
     }
 
+    public static final boolean EXTERNAL_DIR = false;
+
+    private BoxStore boxStore;
+
     @Override
     public void onCreate() {
         super.onCreate();
         appContext = getApplicationContext();
+        // init ObjectBox
+        boxStore = MyObjectBox.builder().androidContext(this).build();
+        if (BuildConfig.DEBUG) {
+            new AndroidObjectBrowser(boxStore).start(this);
+        }
     }
 }
--- flowmemo/src/main/java/me/morirain/dev/flowmemo/bean/Notes.java
+++ flowmemo/src/main/java/me/morirain/dev/flowmemo/bean/Notes.java
@@ -3,16 +3,26 @@ package me.morirain.dev.flowmemo.bean;
 
 import android.arch.lifecycle.MutableLiveData;
 
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.annotation.Transient;
+
 /**
  * Created by morirain on 2018/6/2.
  * E-Mail Address：morirain.dev@outlook.com
  */
 
-
+@Entity
 public class Notes{
 
+    @Id
+    long boxId;
+
+    @Transient
     public MutableLiveData<String> noteLabel = new MutableLiveData<>();
+    @Transient
     public MutableLiveData<String> noteContent = new MutableLiveData<>();
+    @Transient
     public MutableLiveData<String> noteLastUpdateTime = new MutableLiveData<>();
 
     public Notes(String noteLabel, String noteContent, String noteLastUpdateTime) {
